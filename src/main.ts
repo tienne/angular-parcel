9a3d208098e4f39611b2630a9996a793ce9c03d1
tienne <tienne89@gmail.com>
2018-02-02 23:58:08 +0900

    parcel-plugin-typescript -> parcel-plugin-angular

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {AppModule} from './app/app.module';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
+if (process.env.NODE_ENV === 'production') {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
